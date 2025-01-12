899c736d323ec698d01e395f997e04800c6bbf8e
Alex Zasorin <zasorin.alek@gmail.com>
2025-01-12 13:37:20 -0800

    feat(common): add peak memory measurement

--- src/aoc_2024/measure.py
+++ src/aoc_2024/measure.py
@@ -1,4 +1,5 @@
 from time import perf_counter
+import tracemalloc
 from typing import Callable, ParamSpec, TypeVar
 
 
@@ -15,19 +16,37 @@ def format_time(seconds: float) -> tuple[float, str]:
         return seconds / 60, "minutes"
 
 
+def format_memory(bytes: int) -> tuple[float, str]:
+    if bytes < 1024**2:  # Less than 1 KiB
+        return bytes, "bytes"
+    elif bytes < 1024**3:  # Less than 1 MiB
+        return bytes / 1024, "KiB"
+    elif bytes < 1024**4:  # Less than 1 GiB
+        return bytes / 1024**3, "MiB"
+    else:
+        return bytes / 1024**4, "GiB"
+
+
 P = ParamSpec("P")
 R = TypeVar("R")
 
 
 def measure(func: Callable[P, R]) -> Callable[P, R]:
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        tracemalloc.start()
         start = perf_counter()
+
         result = func(*args, **kwargs)
+
         end = perf_counter()
+
+        memory = format_memory(tracemalloc.get_traced_memory()[1])
+        tracemalloc.stop()
 
         time_taken = end - start
         value, unit = format_time(time_taken)
         print(f"Calculated in {value:.2f} {unit}")
+        print(f"Peak memory used: {memory[0] / 1024:.2f} {memory[1]}")
 
         return result
 
